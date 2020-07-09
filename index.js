d8ab5475a57cad5c11eaad0ccf0dc522389d3b71
jdavis3333 <jdavis3333@gmail.com>
2020-07-08 20:40:26 -0700

    started questions array

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
+const questions = [
+    {
+        type: "input",
+        message: "enter github name",
+        name: "githubName"
+    },
+    {
+        type: "list",
+        message: "",
+        name: "badge",
+        choices: ["MIT", "ISC", "GPL", "BSD"]
+    },
 
+];

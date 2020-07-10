a056bc7a5af229c92ae13b64f2c997a22b214c17
jdavis3333 <jdavis3333@gmail.com>
2020-07-09 21:13:42 -0700

    completed minimum questions array

--- index.js
+++ index.js
@@ -5,50 +5,81 @@ const questions = [
     {
         type: "input",
         message: "What is the title of your project?",
-        name: "title"
+        name: "title",
     },
     {
         type: "input",
-        message: "enter github name",
-        name: "githubName"
+        message: "What is your github username?",
+        name: "githubName",
+    },
+    {
+        type: "input",
+        message: "Provide a brief description of the project",
+        name: "description",
+    },
+    {
+        type: "",
+        message: "Table of contents",
+        name: "table",
+    },
+    {
+        type: "input",
+        message: "A step by step series of examples that tell you how to get a development env running",
+        name: "installation",
+    },
+    {
+        type: "input",
+        message: "provide instructions on how the application is to be used",
+        name: "usage",
     },
     {
         type: "list",
-        message: "",
+        message: "Type of license?",
         name: "badge",
-        choices: ["MIT", "ISC", "GPL", "BSD"]
+        choices: ["MIT", "ISC", "GPL", "BSD"],
+    },
+    {
+        type: "input",
+        message: "List any other contributors to this project",
+        name: "contributors",
+    },
+    {
+        type: "input",
+        message: "Tests?",
+        name: "tests",
     },
 
+
 ];
-function getUserInputs(){
+function getUserInputs() {
     inquirer.prompt(questions)
-    .then(function(response){
-        console.log(response);
-        var readmeText = `
+        .then(function (response) {
+            console.log(response);
+            var readmeText = `
 # Project title: ${response.title} 
 ## Project description
-${} 
+${response.description} 
 ## Table of Contents
-${}
+${response.table}
 ### Installation
-${}
+${response.installation}
 ### Usage
-${}
+${response.usage}
 ### License
-![GitHub license](https://img.shields.io/badge/license-${response.badge}-blue.svg)
+This project is licensed under the ![GitHub license](https://img.shields.io/badge/license-${response.badge}-blue.svg).
 ### Contributing
-${}
+${response.contributors}
 ### Tests
-${}
+${response.usage}
 ###Questions
 ### GitHub: ${response.githubName}  
 
 
         `
-        fs.writeFileSync("newREADME.md", readmeText, function(){
-            console.log("readme generated")
+            fs.writeFileSync("newREADME.md", readmeText, function () {
+                console.log("readme generated")
+            })
         })
-    })
 }
 getUserInputs();
 // function writeToFile(fileName, data) {
